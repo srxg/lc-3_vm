12d303dd8794127e0b0f0c72d61f370c0a717a0b
srxg <130320493+srxg@users.noreply.github.com>
2023-05-16 17:33:31 +0100

    Set up registers

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ This VM simulates the fictional computer "LC-3"
 65,536 memory locations storing 16-bit values (2^16 : maximum accessible locations using a 16-bit unsigned int)
 So, our memory is limited to 128KB. Pretty small.
 Use a simple array for this.
+
+### Registers
+We'll use 10 registers. 8 are general purpose, 1 as the program counter (R_PC), and 1 for the condition flags (R_COND).
+
--- lc3_vm_win.h
+++ lc3_vm_win.h
@@ -6,13 +6,35 @@
 #define MEM_MAX (1 << 16)
 #endif
 
-uint16_t memory[MEM_MAX];
-
 /**
  * Registers
  * LC-3 has 10 registers, 16 bits each
- * The majority of these will be general purpose
+ * 8 of these will be general purpose, 1 for the program
+ * counter (R_PC), and 1 for the condition flags (COND).
+ * Theory reminders:
+ *      - Program counter is just an unsigned
+ *        integer which is the next instruction in memory to execute
  * 
+ *      - The condition flags give us info about the previous calculation
 */
+
+enum {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, // the program counter
+    R_COND,
+    R_COUNT // not a register
+};
+
+uint16_t memory[MEM_MAX];
+uint16_t reg[R_COUNT];
+
+
 
 #endif
